3232c690cb00a7374069267521d4881467bf026d
nicogithub05 <nicolasgithub@gmail.com>
2024-11-28 14:51:13 -0500

    generando colores personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./public/index.html', './src/**/*.{html,js}'],
   theme: {
+      colors: {
+        primary: '#00FF00',
+        secondary: '#FF00FF',
+        terciary: 'blue',
+        cuarter: '#600060',
+        quintuple: '#007000'
+      },
       extend: {},
   },
   plugins: [],
