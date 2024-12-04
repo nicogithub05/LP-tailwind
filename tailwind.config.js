da6950c26318ec29c7fd34b42fca0796d9835424
nicogithub05 <nicolasgithub@gmail.com>
2024-12-04 09:47:02 -0500

    agregando imagenes del proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,36 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/index.html', './src/**/*.{html,js}'],
-  theme: {
-      colors: {
-        primary: '#00FF00',
-        secondary: '#FF00FF',
-        terciary: 'blue',
-        cuarter: '#600060',
-        quintuple: '#007000'
-      },
-      extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{html,js}", "./public/*.{html,js}"],
+	theme: {
+		fontFamily: {
+			Montserrat: ['"Montserrat"', "sans-serif"],
+		},
+		colors: {
+			primary: "#CC2D4A",
+			secondary: "#8FA206",
+			tertiary: "#61AEC9",
+      cuarter: '#600060',
+      quintuple: '#007000'
+		},
+		extend: {
+			sanFrancisco: "url('../img/sanFrancisco.jpg')",
+			sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+			yosemite: "url('../img/yosemite.jpg')",
+			LA: "url('../img/LA.jpg')",
+			seattle: "url('../img/seattle.jpg')",
+			new_york: "url('../img/new_york.jpg')",
+			norway: "url('../img/norway.jpg')",
+			sydney: "url('../img/sydney.jpg')",
+			miami: "url('../img/miami.jpg')",
+			switzerland: "url('../img/switzerland.jpg')",
+			bali: "url('../img/bali.jpg')",
+			norway: "url('../img/norway.jpg')",
+			chicago: "url('../img/chicago.jpg')",
+			europe: "url('../img/europe.jpg')",
+			iceland: "url('../img/iceland.jpg')",
+		},
+	},
+	plugins: [
+	],
 };
